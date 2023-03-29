834207bd56dbef7ff48f5e45dbee55fe1f60b302
mishra4amit4@gmail.com <mishra4amit4@gmail.com>
2023-03-29 21:01:30 +0530

    adding more methods

--- src/test/java/appium/HelperClass.java
+++ src/test/java/appium/HelperClass.java
@@ -1,15 +1,32 @@
 package appium;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 
 import static appium.BaseScript.driver;
 
 public class HelperClass {
 
-    public void click(By element){
+    public void click(By element) {
         driver.findElement(element).click();
     }
-    public void sendKeys(By element, String value){
+
+    public void sendKeys(By element, String value) {
         driver.findElement(element).sendKeys(value);
+    }
+
+    public void pressBackButton(int clicks) {
+        for (int i = 0; i < clicks; i++) {
+            driver.navigate().back();
+        }
+    }
+
+    public boolean elementIsVisible(By element) {
+        try {
+            driver.findElement(element);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
     }
 }
